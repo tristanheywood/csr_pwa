5c80cb5d4a11e4b68673631f9b13b3b2bb57c6fd
Tristan Heywood <they2559@uni.sydney.edu.au>
2020-12-29 14:12:00 +1100

    get backend working (when started separately using: cd sotcat_electron; python .\src\sotcat_backend\server.py)

---
+++ img.py
@@ -0,0 +1,146 @@
+from typing import List
+
+from io import BytesIO
+import tkinter
+import tkinter.filedialog
+import base64
+import os
+
+import numpy as np
+from skimage import data, io, filters, draw
+from PIL import Image as PILImage
+
+
+class Image:
+
+    fname: str
+    data: np.ndarray
+
+    def __init__(self, data = None, fname=None):
+        if data is None:
+            self.data = io.imread('./data/sample.png')
+        else:
+            self.data = data
+
+        self.fname = fname
+
+    def add_circle(self, centerR, centerC, radius):
+        rr, cc = draw.circle_perimeter(centerR, centerC, radius)
+
+        self.data[rr, cc] = [255, 0, 0]
+
+    def get_circle_context(self, cr, cc, r, s=3):
+        newIm = np.copy(self.data)
+        row, col = draw.circle_perimeter(cr, cc, r)
+        newIm[row, col] = [255, 0, 0]
+
+        return Image(newIm[
+            max(cr - s*r, 0): min(cr + s*r, newIm.shape[0]),
+            max(cc - s*r, 0): min(cc + s*r, newIm.shape[1]),
+            :
+        ])
+
+    def get_circle_colour(self, cr, cc, r):
+        rows, cols = draw.disk((cr, cc), r)
+
+        # colour = np.mean(self.data[rows, cols], axis=(0, 1))
+        colour = np.mean(self.data[rows, cols], axis=0)
+
+        return colour
+
+    def get_colour_display(self, cr, cc, r):
+        colour = self.get_circle_colour(cr, cc, r)
+
+        rows, cols = draw.disk((cr, cc), r)
+
+        im = np.full((2*r, 2*r, 3), 255, dtype=self.data.dtype)
+
+        irows, icols = draw.disk((r, r), r)
+
+        im[irows, icols] = self.data[rows, cols]
+
+        im[:,:r] = colour
+
+        return Image(im)
+
+    def to_png_bytes(self):
+
+        with BytesIO() as stream:
+
+            pIm = PILImage.fromarray(self.data)
+            pIm.save(stream, format='PNG')
+
+            return stream.getvalue()
+
+    def to_b64_png(self):
+        return base64.b64encode(self.to_png_bytes()).decode('ascii')
+
+    def show(self):
+        io.imshow(self.data)
+        io.show()
+
+if __name__ == '__main__':
+    img = Image()
+    cr = 294
+    cc = 484
+    r = 73
+
+class ImageFolder:
+
+    downsample: int = 2
+
+    def __init__(self, dir_path: str, images: List[Image]):
+        self.dir_path = dir_path
+        self.images = images
+
+    @classmethod
+    def from_gui_folder_selection(cls):
+        root = tkinter.Tk()
+        root.attributes("-topmost", True)
+        root.lift()
+        root.focus_force()
+        root.withdraw()
+
+        res = tkinter.filedialog.askdirectory(parent=root)
+        root.destroy()
+        print(res)
+        scans = os.listdir(res)
+        print(scans)
+
+        # thumbnails = []
+        images = []
+
+        for scan in scans:
+            fname = res + '/' + scan
+            print('opening: ', fname)
+            try:
+                scanData = io.imread(fname)
+            except:
+                print('Failed to open', fname)
+                continue
+
+            print('finished imread')
+            print('WARNING: IMAGES ARE BEING DOWNSIZED')
+            images.append(Image(scanData[::cls.downsample, ::cls.downsample], scan))
+            # scanThumb = transform.resize(scanData, (scanData.shape[0] // 10, scanData.shape[1] // 10), anti_aliasing=True)
+            # scanThumb = Image(np.asarray(scanData[::8, ::8], order='C'))
+            # thumbnails.append({
+            #     'fileName': scan,
+            #     'img': base64.b64encode(scanThumb.to_png_bytes()).decode('ascii'),
+            # })
+
+        return cls(res, images)
+
+    def get_thumbnails(self):
+        return [
+            {
+                'fileName': img.fname,
+                'img': base64.b64encode(
+                    Image(np.asarray(img.data[::8, ::8], order='C')).to_png_bytes()
+                ).decode('ascii')
+            }
+            for img in self.images
+        ]
+
+    def get_image_with_fname(self, fname: str) -> Image:
+        return [i for i in self.images if i.fname == fname][0]
---
+++ server.py
@@ -0,0 +1,202 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import urllib.parse
+from io import BytesIO
+import os
+import json
+import base64
+import webbrowser
+import tkinter
+import tkinter.filedialog
+import ctypes
+from numpy.lib.type_check import imag
+
+import pyperclip
+from skimage import io, transform
+import numpy as np
+
+# ide likes .img imports, python does not
+if False is True:
+    from .img import Image, ImageFolder
+else:
+    from img import Image, ImageFolder
+
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+
+    image: Image = None
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # self.image: Image = None
+
+    def do_GET(self):
+        # import pdb; pdb.set_trace();
+        print('Get to path: {}'.format(self.path))
+
+        if (self.path == '/'):
+            self.do_get_root()
+        elif (self.path == '/pic'):
+            self.do_get_pic()
+        elif (self.path == '/open_folder'):
+            self.do_open_folder()
+        elif (self.path.startswith('/select_scan')):
+            self.do_get_selected_scan()
+        else:
+            print('path unrecognized')
+
+    def do_get_selected_scan(self):
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.end_headers()
+
+        qs = urllib.parse.parse_qs(urllib.parse.urlparse(self.path).query)
+        print(qs)
+
+        fname = qs['fname'][0]
+
+        global imageFolder, colours
+
+        colours = []
+
+        self.wfile.write(json.dumps(
+            {'imgData': imageFolder.get_image_with_fname(fname).to_b64_png()}
+        ).encode('utf-8'))
+
+    def do_get_pic(self):
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.end_headers()
+
+        img = open('./data/sample.png', 'rb').read()
+        img64 = base64.b64encode(img).decode('ascii')
+        # import pdb; pdb.set_trace()
+
+        self.wfile.write(json.dumps(
+            {'meme': True, 'imgData': img64}).encode('utf-8'))
+
+    def do_open_folder(self):
+
+        global imageFolder
+        imageFolder = ImageFolder.from_gui_folder_selection()
+
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.end_headers()
+
+        self.wfile.write(json.dumps(
+            {'hasImages': len(imageFolder.images) > 0, 'thumbnails': imageFolder.get_thumbnails()}).encode('utf-8'))
+
+    def do_get_root(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+
+        js = open('../frontend/app.tsx', 'rt').read()
+
+        html = '''<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <title>Hello World</title>
+    <script src="https://unpkg.com/react@16/umd/react.development.js"></script>
+    <script src="https://unpkg.com/react-dom@16/umd/react-dom.development.js"></script>
+
+    <!-- Don't use this in production: -->
+    <script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
+  </head>
+  <body>
+    <div id="root"></div>
+    <script type="text/babel">
+      {}
+    </script>
+    <!--
+      Note: this page is a great way to try React but it's not suitable for production.
+      It slowly compiles JSX with Babel in the browser and uses a large development build of React.
+
+      Read this section for a production-ready setup with JSX:
+      https://reactjs.org/docs/add-react-to-a-website.html#add-jsx-to-a-project
+
+      In a larger project, you can use an integrated toolchain that includes JSX instead:
+      https://reactjs.org/docs/create-a-new-react-app.html
+
+      You can also use React without JSX, in which case you can remove Babel:
+      https://reactjs.org/docs/react-without-jsx.html
+    -->
+  </body>
+</html>'''.format(js)
+
+        # html = open('../frontend/index.html', 'rt').read()
+
+        self.wfile.write(bytes(html, 'utf8'))
+        # self.wfile.write(b'Hello, world!')
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        body = self.rfile.read(content_length)
+
+        body = json.loads(body)
+        print('POST to ', self.path)
+        print('body:', body)
+
+        # global image
+        # self.image = image
+
+        global imageFolder
+        image = imageFolder.get_image_with_fname(body['fname'])
+
+        # self.image.add_circle(int(body['center']['y']), int(body['center']['x']), int(body['radius']))
+        # self.image.show()
+        cr = int(body['center']['y'])
+        cc = int(body['center']['x'])
+        r = int(body['radius'])
+
+        im = image.get_circle_context(int(body['center']['y']), int(
+            body['center']['x']), int(body['radius']))
+
+        contextPNG = im.to_png_bytes()
+        img64 = base64.b64encode(contextPNG).decode('ascii')
+
+        colour = image.get_circle_colour(cr, cc, r)
+        comparePNG = image.get_colour_display(cr, cc, r).to_png_bytes()
+        compare64 = base64.b64encode(comparePNG).decode('ascii')
+
+        global colours
+        colours.append([int(x) for x in colour])
+
+        clipboard = '\n'.join('\t'.join(str(component) for component in _colour)
+                              for _colour in colours)
+        pyperclip.copy(
+            clipboard
+        )
+
+        # import pdb; pdb.set_trace();
+
+        self.send_response(200)
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.end_headers()
+        response = BytesIO()
+        # response.write(b'This is POST request. ')
+        # response.write(b'Received: ')
+        response.write(json.dumps({
+            "circContext": img64,
+            "meanColour": [int(x) for x in colour],
+            "colourCompare": compare64,
+            "clipboardContent": clipboard,
+        }).encode('utf-8'))
+        self.wfile.write(response.getvalue())
+
+
+# fix tkinter bad dpi scaling
+ctypes.windll.shcore.SetProcessDpiAwareness(1)
+
+image = Image()
+imageFolder = None
+colours = []
+httpd = HTTPServer(('localhost', 8000), SimpleHTTPRequestHandler)
+print('Serving...')
+# webbrowser.open('http://localhost:8000')
+httpd.serve_forever()
