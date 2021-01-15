fc5413de44212fea58510edac91b153053b91e0d
Tristan Heywood <tristanheywood@hotmail.com>
2021-01-15 16:43:15 +1100

    add protobuf and get app working in dev mode

---
+++ build_protobuf.ps1
@@ -0,0 +1,5 @@
+protoc --plugin=protoc-gen-ts=..\..\node_modules\.bin\protoc-gen-ts.cmd --js_out=import_style=commonjs,binary:..\sotcat_frontend\protobuf_js --ts_out=..\sotcat_frontend\protobuf_js types.proto
+
+# protoc --python_out=..\sotcat_backend\protobuf_py types.proto
+
+protoc --python_betterproto_out=..\sotcat_backend\protobuf_py types.proto
