ba3b3d7ebfe462357721854acc522cd0edc5b413
Tristan Heywood <tristanheywood@hotmail.com>
2021-02-28 19:53:18 +1100

    wip getting working

---
+++ src/csr.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SotcatContainer from './csr_frontend/Sotcat'
+
+class ColorimetricSensorReader extends React.Component<{}, {}> {
+
+    render() {
+        return (
+            <SotcatContainer/>
+        )
+    }
+}
+
+export default ColorimetricSensorReader;
